0a2bad5a0e5e04129abdc3e693821b937cca8df4
asif279 <asifofficial8130@gmail.com>
2022-10-15 15:05:21 +0600

    add

--- interviewQs.js
+++ interviewQs.js
@@ -1,14 +1,28 @@
 //Swap variable
 function swapVariable(num1,num2){
     //5,7
-    var temp = num1;//5
+   
+   //wuth Temporary variable
+    /* var temp = num1;//5
     num1= num2;// b=5
-    num2= temp;
+    num2= temp;*/
+    /*
+    //swap without temp
+    num1= num1 +num2;
+    num2 =num1-num2;
+    num1= num1-num2;
+
+    */
+   /*
+   Destructing Javascript Method
+   [num1,num2] =[num2,num1];
+   */
+
 
     return [num1 ,num2
     ];
 }
 var result = swapVariable(5,7);
  console.log(result);
- //swap without temp
+ 
  // destructing
